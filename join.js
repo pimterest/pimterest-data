b27b47f4d30c6785fc2e75fb5ebb547d08952ada
Pim Snel <post@pimsnel.com>
2022-02-04 16:29:33 +0100

    indenting

--- join.js
+++ join.js
@@ -7,55 +7,55 @@ const directoryPath = path.join(__dirname, "telegram")
 let jsonData = [];
 
 async function showPreview(link){
-	const previewData = await linkPreviewGenerator( link);
-	console.log(previewData);
+  const previewData = await linkPreviewGenerator( link);
+  console.log(previewData);
 }
 
 async function walkTelegramFiles(){
-	console.log(directoryPath)
-	fs.readdir(directoryPath,async function(err, files) {
-		if (err) {
-			console.log("Error getting directory information.")
-		} else {
-			files.forEach(async function(file) {
+  console.log(directoryPath)
+  fs.readdir(directoryPath,async function(err, files) {
+    if (err) {
+      console.log("Error getting directory information.")
+    } else {
+      files.forEach(async function(file) {
 
-				console.log(file)
-				if(file !=".keep"){
+        console.log(file)
+        if(file !=".keep"){
 
-					var  basefile = file.replace(/\.[^/.]+$/, "")
-					var filearr = basefile.split("_");
+          var  basefile = file.replace(/\.[^/.]+$/, "")
+          var filearr = basefile.split("_");
 
-					let itemArr = {};
-					itemArr.permid = file;
-					itemArr.date = filearr[0];
-					itemArr.type = filearr[2];
-					itemArr.source = "telegram";
+          let itemArr = {};
+          itemArr.permid = file;
+          itemArr.date = filearr[0];
+          itemArr.type = filearr[2];
+          itemArr.source = "telegram";
 
-					if(itemArr.type == "link"){
-						await fs.readFile (path.join(directoryPath, file), 'utf8',async function (err, link) {
-							if (err) {
-								console.log(err);
-								process.exit(1);
-							}
-							itemArr.url = link;
-							itemArr.preview = await showPreview(link);
-						});
+          if(itemArr.type == "link"){
+            await fs.readFile (path.join(directoryPath, file), 'utf8',async function (err, link) {
+              if (err) {
+                console.log(err);
+                process.exit(1);
+              }
+              itemArr.url = link;
+              itemArr.preview = await showPreview(link);
+            });
 
-					}
+          }
 
-					if(filearr[2] == "image"){
-						itemArr.image = file;
-					}
+          if(filearr[2] == "image"){
+            itemArr.image = file;
+          }
 
-					console.log(file)
-					jsonData.push(itemArr);
-				}
-			})
+          console.log(file)
+          jsonData.push(itemArr);
+        }
+      })
 
-			console.log(jsonData);
+      console.log(jsonData);
 
-		}
-	})
+    }
+  })
 
 
 }
@@ -70,9 +70,9 @@ const data = JSON.stringify(jsonData);
 
 // write JSON string to a file
 fs.writeFile('out/telegram.json', data, (err) => {
-	if (err) {
-		throw err;
-	}
-	console.log("JSON data is saved.");
+  if (err) {
+    throw err;
+  }
+  console.log("JSON data is saved.");
 });
 
